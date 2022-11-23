d6c8242dfb0e2a1b24ea48837a791d30c27fec8a
Alexander Bogachenkov <sashako4etov@yandex.ru>
2022-11-23 15:43:03 +0300

    commit 3

--- scripts/index.js
+++ scripts/index.js
@@ -1,91 +1,84 @@
-const popup = document.querySelector('.popup'); // Фон попап окна
-const openPopupButton = document.querySelector('.edit-button'); // Кнопки для показа окна
-const closePopupButton = document.querySelector('.popup__close-button'); // Кнопка для скрытия окна
+const popup = document.querySelector(".popup"); // Фон попап окна
+const openPopupButton = document.querySelector(".profile__edit-button"); // Кнопки для показа окна
+const closePopupButton = document.querySelector(".popup__close-button"); // Кнопка для скрытия окна
 
-const profailName = document.querySelector('.profail__name').innerHTML; 
-const profailDescription = document.querySelector('.profail__description').innerHTML; 
+// Считываем данные профайла
+const profileName = document.querySelector(".profile__name");
+const profileDescription = document.querySelector(".profile__description");
 
-document.querySelector('.popup__profail_name').value = profailName;
-document.querySelector('.popup__profail_description').value = profailDescription;
+// Получаем значение полей jobInput и nameInput
+const nameInput = document.querySelector(".popup__profile_type_name");
+const jobInput = document.querySelector(".popup__profile_type_description");
 
 
-
-function closeModal () {
-    //В брифе написано - Чтобы закрыть попап, 
-    //удаляйте у него модификатор popup_opened
-
-    popup.classList.remove('popup_opened');  
-    document.body.style.overflow = '';
-    document.body.style.paddingRight = '';
-};
-
+// Прикрепляем обработчик к форме:
+// он будет следить за событием “submit” - «отправка»
+const formElement = document.querySelector(".popup__content");
 
 
 // Обработчик «отправки» формы, хотя пока
 // она никуда отправляться не будет
-function formSubmitHandler (evt) {
-    evt.preventDefault();  
+function formSubmitHandler(evt) {
 
-
-    // Получите значение полей jobInput и nameInput из свойства value
-    const nameInput = document.querySelector('.popup__profail_name').value; 
-    const jobInput = document.querySelector('.popup__profail_description').value; 
-
-   
-    document.querySelector('.profail__name').textContent = nameInput;
-    document.querySelector('.profail__description').textContent = jobInput;
-
-    closeModal();    
-};
+  evt.preventDefault();
+  
+  profileName.textContent = nameInput.value;
+  profileDescription.textContent = jobInput.value;
+  closeModal();
+}
 
 
 
- 
-openPopupButton.addEventListener('click', (e) => {        
-    
-        //В брифе написано - Чтобы попап открывался, добавляйте ему модификатор 
-        //popup_opened с одним-единственным правилом.
-        popup.classList.add('popup_opened');
-
-        if(window.innerWidth > 320) {
-            
-        document.body.style.paddingRight = window.innerWidth - document.querySelector('.page').offsetWidth + 'px';
-           
-    }
-
-        document.body.style.overflow = 'hidden';
-    });
+// Закрываем окно
+function closeModal() {
+  popup.classList.remove("popup_opened");
+  document.body.style.overflow = "";
+  document.body.style.paddingRight = "";
+}
 
 
 
-closePopupButton.addEventListener('click', closeModal); 
+//Слушаем форму - кнопку submit
+formElement.addEventListener("submit", formSubmitHandler);
 
 
 
-document.addEventListener('click', (e) => { 
-    if(e.target === popup) {         
-        closeModal();        
-    }
+// Открываем окно редактирования профайлы
+  openPopupButton.addEventListener("click", (e) => {
+  popup.classList.add("popup_opened");
+
+  // Вставляем данные профайла в попап окно
+  document.querySelector(".popup__profile_type_name").value = profileName.textContent;
+  document.querySelector(".popup__profile_type_description").value = profileDescription.textContent;
+
+
+  // Не скролим при 320px  
+  if (window.innerWidth > 320) {
+    document.body.style.paddingRight =
+      window.innerWidth - document.querySelector(".page").offsetWidth + "px";
+  }
+
+  document.body.style.overflow = "hidden";
 });
 
+
+// Закрываем попап через крестик
+closePopupButton.addEventListener("click", closeModal);
+document.addEventListener("click", (e) => {
+  if (e.target === popup) {
+    closeModal();
+  }
+});
 
 
 
 //закрытие окна по escape
 document.addEventListener("keydown", (e) => {
-    if (e.code === "Escape" && popup.classList.contains('popup_opened')) {
-        closeModal();        
-    }
-});   
+  if (e.code === "Escape" && popup.classList.contains("popup_opened")) {
+    closeModal();
+  }
+});
 
 
-
-// Прикрепляем обработчик к форме:
-// он будет следить за событием “submit” - «отправка»
-const formElement = document.querySelector('.popup__content');
-formElement.addEventListener('submit', formSubmitHandler); 
-
-
-   
 
 
